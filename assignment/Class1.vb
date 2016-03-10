7e49d23b4a70acc0425364ff8ee3724f4e17e076
mamaome95 <andd181995vn@gmail.com>
2016-03-10 14:12:29 +0700

    commit

---
+++ assignment/Class1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Public Class Class1
+
+    Public Function Loadsanpham() As DataSet
+        'Dim chuoiketnoi As String = "Data Source=PC243\;Initial Catalog=ANDDPS03332;Integrated Security=True"
+        Dim chuoiketnoi As String = "workstation id=final-ps03332.mssql.somee.com;packet size=4096;user id=demops03332_SQLLogin_2;pwd=ico8vh59ew;data source=final-ps03332.mssql.somee.com;persist security info=False;initial catalog=final-ps03332"
+        Dim conn As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim LoadSP As New SqlDataAdapter("select MaSP as 'Mã sản phẩm',TenSP as 'Tên sản phẩm',Soluong as 'Số lượng', Soluong * Dongia as 'Thành Tiền' from SANPHAM1", conn)
+        Dim db As New DataSet
+        conn.Open()
+        LoadSP.Fill(db)
+        conn.Close()
+        Return db
+    End Function
+End Class
